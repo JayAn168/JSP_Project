6f0b2bd9951de3e1388cff57deb53c25d0e48bb8
JayAn168 <mystar999@naver.com>
2022-07-28 11:54:45 +0900

    swiper 이벤트  수정

--- js/main.js
+++ js/main.js
@@ -47,10 +47,14 @@ function append() {
     .start();
 }
 
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
+let swiper = new Swiper(".mySwiper", {
+  loop: true, // 무한반복
+  loopAdditionalSlides: 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
   autoplay: {
-    delay: 4000,
+    // 자동재생
+    delay: 4000, // 시간 설정
+    disableOnInteraction: false,
+    // false로 설정 시 스와이프 후 자동 재생이 비활성화 되지 않음
   },
   pagination: {
     el: ".swiper-pagination",
@@ -61,65 +65,35 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-function changebgc() {
-  const pagenum = document.getElementsByClassName("swiper-slide-active");
-  let num = pagenum[0].innerText;
+swiper.on("slideChange", function () {
+  let num = swiper.activeIndex; // 슬라이드 될때마다 인덱스번호, 직접 오차 확인하면서 해야함
   const bgc = document.getElementById("swiper_ad");
-  if (num == 1) {
+  if (num == 2) {
     bgc.style.transition = "all 0.5s";
     bgc.style.backgroundColor = "rgb(0, 60, 215)";
-  } else if (num == 2) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(255, 153, 0)";
   } else if (num == 3) {
     bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(255, 113, 139)";
+    bgc.style.backgroundColor = "rgb(255, 153, 0)";
   } else if (num == 4) {
     bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(33, 34, 36)";
+    bgc.style.backgroundColor = "rgb(255, 113, 139)";
   } else if (num == 5) {
     bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(150, 96, 240)";
+    bgc.style.backgroundColor = "rgb(33, 34, 36)";
   } else if (num == 6) {
     bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(12, 69, 65)";
+    bgc.style.backgroundColor = "rgb(150, 96, 240)";
   } else if (num == 7) {
+    bgc.style.transition = "all 0.5s";
+    bgc.style.backgroundColor = "rgb(12, 69, 65)";
+  } else if (num == 8) {
+    bgc.style.transition = "all 0.5s";
+    bgc.style.backgroundColor = "rgb(55, 17, 108)";
+  } else if (num == 9) {
+    bgc.style.transition = "all 0.5s";
+    bgc.style.backgroundColor = "rgb(0, 60, 215)";
+  } else if (num == 1) {
     bgc.style.transition = "all 0.5s";
     bgc.style.backgroundColor = "rgb(55, 17, 108)";
   }
-}
-let interval = setInterval(changebgc, 300);
-
-const nextbtn = document.getElementById("nextbtn");
-const prevbtn = document.getElementById("prevbtn");
-prevbtn.addEventListener("click", clickBtn);
-nextbtn.addEventListener("click", clickBtn);
-function clickBtn() {
-  clearInterval(interval);
-  const pagenum = document.getElementsByClassName("swiper-slide-active");
-  const num = pagenum[0].innerText;
-  const bgc = document.getElementById("swiper_ad");
-  if (num == 1) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(0, 60, 215)";
-  } else if (num == 2) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(255, 153, 0)";
-  } else if (num == 3) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(255, 113, 139)";
-  } else if (num == 4) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(33, 34, 36)";
-  } else if (num == 5) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(150, 96, 240)";
-  } else if (num == 6) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(12, 69, 65)";
-  } else if (num == 7) {
-    bgc.style.transition = "all 0.5s";
-    bgc.style.backgroundColor = "rgb(55, 17, 108)";
-  }
-}
+});
